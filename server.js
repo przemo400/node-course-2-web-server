38bc4df8413b793857135067f630219a97e7b299
Przemo400 <przemek@MacBook-Pro.localdomain>
2018-02-19 18:26:19 +0100

    Tets

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); 
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
